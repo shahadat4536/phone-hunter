4ee9f3903a96b47de722f487f6214f50410867a5
Shahadat Hossain <shahadathossain4536@gmail.com>
2022-03-01 08:43:28 +0600

    erro and div html done

--- js/index.js
+++ js/index.js
@@ -8,35 +8,47 @@ const searchPhone = () => {
     const inputValueText = inputValue.value;
     // console.log(inputValueText)
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue.value}`
-    // console.log(url)
+    console.log(url)
     if (isNaN(inputValue.value) == false) {
         error.innerText = "Please enter text"
+        phoneFullDetailsCard.innerHTML = '';
 
     } else {
+        phonePreDetails.innerHTML = '';
+        error.innerText = '';
+        phoneFullDetailsCard.innerHTML = '';
         fetch(url)
             .then(res => res.json())
-            .then(data => displayPhone(data.data))
+            .then(data => displayPhone(data.data.slice(0, 20)))
     }
 }
 const phonePreDetails = document.getElementById('phone-pre-details')
 const displayPhone = (phones) => {
-    // console.log(phones)
+    console.log('ggg', phones)
 
-    for (const phone of phones) {
-        // console.log(phone)
-        const div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML = `
-        <div class="card h-100">
-                <img src="${phone.image}" class="card-img-top mx-auto w-75" alt="...">
-                <div class="card-body">
-                    <h3 class="card-title"> ${phone.phone_name}</h3>
-                    <p>Brand: ${phone.brand}</p>
-                </div> 
-                <button onclick="phoneFullDetails('${phone.slug}')" type="button" class="btn btn-primary">Details</button>
-        </div>`
-        phonePreDetails.appendChild(div);
-    };
+
+    if (phones.length == 0) {
+        error.innerText = "Please enter text0000000000"
+        phonePreDetails.innerHTML = '';
+
+        // alert('55555555555555')
+    } else {
+        for (const phone of phones) {
+            // console.log(phone)
+            const div = document.createElement('div')
+            div.classList.add('col')
+            div.innerHTML = `
+            <div class="card h-100">
+                    <img src="${phone.image}" class="card-img-top mx-auto w-75" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title"> ${phone.phone_name}</h3>
+                        <p>Brand: ${phone.brand}</p>
+                    </div> 
+                    <button onclick="phoneFullDetails('${phone.slug}')" type="button" class="btn btn-primary">Details</button>
+            </div>`
+            phonePreDetails.appendChild(div);
+        };
+    }
 
 };
 
@@ -52,6 +64,7 @@ const phoneFullDetails = (id) => {
 
 const phoneFullDetailsCard = document.getElementById('phone-full-details-card')
 const displayPhoneFullDetails = (info) => {
+    phoneFullDetailsCard.innerHTML = '';
     console.log(info)
     const div = document.createElement('div')
     div.classList.add('card')
@@ -69,12 +82,12 @@ const displayPhoneFullDetails = (info) => {
                     <p class="card-text">Memory: ${info.mainFeatures.memory}</p>
                     <p class="card-text">Storage: ${info.mainFeatures.storage}</p>
                     <p class="card-text">Sensors: ${info.mainFeatures.sensors}</p>
-                    <p class="card-text">Bluetooth: ${info.others.Bluetooth}</p>
-                    <p class="card-text">GPS: ${info.others.GPS}</p>
-                    <p class="card-text">NFC: ${info.others.NFC}</p>
-                    <p class="card-text">Radio: ${info.others.Radio}</p>
-                    <p class="card-text">USB: ${info.others.USB}</p>
-                    <p class="card-text">WLAN: ${info.others.WLAN}</p>
+                    <p class="card-text">Bluetooth: ${info?.others?.Bluetooth ? info.others.Bluetooth : `No Bluetooth`}</p>
+                    <p class="card-text">GPS: ${info?.others?.GPS ? info.others.GPS : `No GPS Data Found`}</p>
+                    <p class="card-text">NFC: ${info?.others?.NFC ? info.others.NFC : `No NFC Data Found`}</p>
+                    <p class="card-text">Radio: ${info?.others?.Radio ? info.others.Radio : `No Radio Data Found`}</p>
+                    <p class="card-text">USB: ${info?.others?.USB ? info.others.USB : `No USB Data Found`}</p>
+                    <p class="card-text">WLAN: ${info?.others?.WLAN ? info.others.WLAN : `No WLAN Data Found`}</p>
                 </div>
             </div>
         </div>`
