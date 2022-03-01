fc3ea71cc4b5b4030c09dedf7cf181ff7d2249e3
Shahadat Hossain <shahadathossain4536@gmail.com>
2022-03-01 20:39:46 +0600

    dddd

--- js/index.js
+++ js/index.js
@@ -3,48 +3,6 @@ let errorMessage = error.innerText;
 const spinner = document.getElementById('spinner')
 const phonePreDetails = document.getElementById('phone-pre-details')
 const phoneFullDetailsCard = document.getElementById('phone-full-details-card')
-const showAll = document.getElementById('show-all')
-
-
-
-
-const showAllPhone = () => {
-    const inputValue = document.getElementById('input-value');
-    const inputValueText = inputValue.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue.value}`
-    console.log(url)
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayAllPhone(data.data))
-}
-
-const displayAllPhone = (phones) => {
-
-    for (const phone of phones) {
-        // console.log(phone)
-        const div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML = `
-        <div class="card h-100">
-                <img src="${phone.image}" class="card-img-top mx-auto w-75 py-5" alt="...">
-                <div class="card-body">
-                    <h3 class="card-title"> ${phone.phone_name}</h3>
-                    <p>Brand: ${phone.brand}</p>
-                </div> 
-                <button onclick="phoneFullDetails('${phone.slug}')"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                Details
-                </button>
-        </div>`
-        phonePreDetails.appendChild(div);
-    };
-}
-
-
-
-
-
-
-
 
 /* search box and button start */
 const searchPhone = () => {
